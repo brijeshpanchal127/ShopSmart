c3f9eeb5ba8a35a52fcc7e34b2abd8ff45e09431
Brijesh Panchal <brijeshpanchal127@gmail.com>
2021-02-03 17:07:23 -0500

    redux applied

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Rating from '../components/Rating';
-import data from '../data';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+
+import { detailsProduct } from '../actions/productActions';
+
+// import data from '../data';
 
 export default function ProductScreen(props) {
-  const product = data.products.find((x) => x._id === props.match.params.id);
-  if (!product) {
-    return <div> Product Not Found</div>;
-  }
+  // const product = data.products.find((x) => x._id === props.match.params.id);
+  // if (!product) {
+  //   return <div> Product Not Found</div>;
+  // }
+  const dispatch = useDispatch();
+  const productId = props.match.params.id;
+  const productDetails = useSelector((state) => state.productDetails);
+  const { loading, error, product } = productDetails;
+
+  useEffect(() => {
+    dispatch(detailsProduct(productId));
+  }, [dispatch, productId]);
+
+
   return (
     <div>
-      <Link to="/">Back to result</Link>
+    {loading ? (
+      <LoadingBox></LoadingBox>
+    ) : error ? (
+      <MessageBox variant="danger">{error}</MessageBox>
+    ) : (
+    <div>
+      <Link to="/" className="link">Back to result</Link>
       <div className="row top">
         <div className="col-2">
-          <img className="large" src={product.image} alt={product.name}></img>
+          <img className="medium" src={product.image} alt={product.name}></img>
         </div>
         <div className="col-1">
           <ul>
@@ -62,6 +84,8 @@ export default function ProductScreen(props) {
         </div>
       </div>
     </div>
+      )}
+      </div>
   );
 }
 
