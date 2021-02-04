8350f4ed31408b47c8ffeeb2ff4c10d222f54c4f
Brijesh Panchal <brijeshpanchal127@gmail.com>
2021-02-04 00:53:03 -0500

    data coming from mongodb done

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,48 @@
 import express from 'express';
-import data from './data.js';
+// import data from './data.js';
+import productRouter from './routers/productRouter.js';
+import mongoose from 'mongoose';
+import userRouter from './routers/userRouter.js';
 
 const app = express();
 
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product Not Found' });
-  }
+// to make connection with mongodb
+
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/shopsmart', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  // to get out deplicated warning
 });
 
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
-});
+// ------------this code is for load static data--------------------
+
+// app.get('/api/products/:id', (req, res) => {
+//   const product = data.products.find((x) => x._id === req.params.id);
+//   if (product) {
+//     res.send(product);
+//   } else {
+//     res.status(404).send({ message: 'Product Not Found' });
+//   }
+// });
+
+// app.get('/api/products', (req, res) => {
+//   res.send(data.products);
+// });
+
+app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
+
+
 
 app.get('/', (req, res) => {
   res.send('Server is ready');
+});
+
+
+// this middleware for error catcher
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
 });
 
 
