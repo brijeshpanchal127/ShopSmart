28130e963498603e5e6087763e08344522077b5c
Brijesh Panchal <brijeshpanchal127@gmail.com>
2021-02-05 00:10:24 -0500

    order details done

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,7 +4,7 @@ import thunk from 'redux-thunk';
 import { productListReducer, productDetailsReducer } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
 import { userRegisterReducer, userSigninReducer } from './reducers/userReducers';
-import { orderCreateReducer } from './reducers/orderReducers';
+import { orderCreateReducer, orderDetailsReducer, } from './reducers/orderReducers';
 
 const initialState = {
   userSignin: {
@@ -34,6 +34,7 @@ const reducer = combineReducers({
   userSignin: userSigninReducer,
   userRegister: userRegisterReducer,
   orderCreate: orderCreateReducer,
+  orderDetails: orderDetailsReducer,
 });
 
 
